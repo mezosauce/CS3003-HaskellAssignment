247ed79f8ead508fed8c7ea458087c0516468662
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-12-09 14:13:48 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,15 +1,24 @@
 module HaskellAssignment where
 
+import Control.Arrow (Arrow (first))
+
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-data Found = Match Int | NoMatch deriving Eq
+
+data Found = Match Int | NoMatch deriving (Eq)
+
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
-findFirst :: Eq a => (a -> Bool) -> [a] -> Found
+
+findFirst (first : rest) = first
+
+findFirst :: (Eq a) => (a -> Bool) -> [a] -> Found
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
+palindrome
+
 palindrome :: [Char] -> Bool
