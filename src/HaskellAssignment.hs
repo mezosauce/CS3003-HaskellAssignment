b8902f026984638652ee5f41e25cf3fdfce7364e
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-12-10 14:44:29 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,16 +1,26 @@
 module HaskellAssignment where
 
-------------------------------------------------
--- findFirst
-------------------------------------------------
 data Found = Match Int | NoMatch deriving (Eq)
 
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 
+------------------------------------------------
+-- findFirst
+------------------------------------------------
+
 findFirst :: (Eq a) => (a -> Bool) -> [a] -> Found
+findFirst needle haystack = findFirstHelper needle haystack 0
+  where
+    findFirstHelper :: (a -> Bool) -> [a] -> Int -> Found
+    findFirstHelper _ [] _ = NoMatch
+    findFirstHelper needle (x : xs) index
+      | needle x = Match index
+      | otherwise = findFirstHelper needle xs (index + 1)
+
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome s = s == reverse s
