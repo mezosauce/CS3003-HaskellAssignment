642d7ae53a4af0f027dbb8cab942f500728d6fce
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-12-11 12:19:57 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -7,7 +7,10 @@ instance Show Found where
   show NoMatch = "No match found!"
 
 ------------------------------------------------
--- findFirst
+-- findFirst:
+-- Take the "needle" -> the number in the list that you are trying to find
+-- Take the "haystack" -> The list of element being traversed through to find the element recursivley
+-- The motion of the function is to use Post-Recrusion to ensure no over memory usage on the stack and to find the element faster
 ------------------------------------------------
 
 findFirst :: (Eq a) => (a -> Bool) -> [a] -> Found
@@ -17,7 +20,8 @@ doFindFirst _ [] _ = NoMatch
 doFindFirst needle (x : xs) index = if (needle x) then Match index else doFindFirst needle xs (index + 1)
 
 ------------------------------------------------
--- palindrome
+-- palindrome:
+-- Take the string S and compare it to its built-in reverse string. Theres really not much to HASKELL about this function urur.
 ------------------------------------------------
 palindrome :: [Char] -> Bool
 palindrome s = s == (reverse s)
